b71daaa3d19220b618b6f6fc71e312b5a413b659
unknown <hetarth002@gmail.com>
2023-07-24 12:12:53 +0530

    add redux-toolkit

--- client/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  const date = new Date();
-  const currentYear = date.getFullYear();
-  return <p className="footer">&copy; Hetarth {currentYear}</p>;
-};
-export default Footer;
